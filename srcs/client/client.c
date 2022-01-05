d0257f56d9c8d9ab1e5a111b71bb5a6c0ee38160
Illarion <pro100illarion@gmail.com>
2022-01-05 20:41:39 +0300

    Added validation of entered files

--- srcs/client/client.c
+++ srcs/client/client.c
@@ -6,7 +6,7 @@
 /*   By: glashli <glashli@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/05 13:36:17 by glashli           #+#    #+#             */
-/*   Updated: 2022/01/05 13:42:15 by glashli          ###   ########.fr       */
+/*   Updated: 2022/01/05 18:57:11 by glashli          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,17 @@ int	main(int argc, char	**argv)
 	if (argc == 3)
 	{
 		pid = ft_atoi(argv[1]);
-		if (pid == 0)
+		if (pid < 1)
+		{
+			ft_putendl_fd("\nError: incorrect PID", 2);
 			exit(EXIT_FAILURE);
+		}		
 		ft_send_data(pid, argv[2]);
 		exit(EXIT_SUCCESS);
 	}
 	else
+	{
+		ft_putendl_fd("\nError: wrong number of arguments", 2);
 		exit(EXIT_FAILURE);
+	}
 }
