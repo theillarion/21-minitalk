0f0c06c4e1a7441f7b6c5edb2208e4f6f80f2569
Illarion <pro100illarion@gmail.com>
2022-01-06 00:10:44 +0300

    Added new function prototypes

--- includes/server.h
+++ includes/server.h
@@ -6,7 +6,7 @@
 /*   By: glashli <glashli@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/05 13:36:32 by glashli           #+#    #+#             */
-/*   Updated: 2022/01/05 20:34:32 by glashli          ###   ########.fr       */
+/*   Updated: 2022/01/05 23:21:36 by glashli          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,14 @@
 # include <stdio.h>
 # include <string.h>
 # include <unistd.h>
+# include <sys/types.h>
 # include <stdlib.h>
 # include <signal.h>
 # include "shared.h"
 
 void	*ft_memset(void	*str, int symbol, size_t len);
+void	ft_putnbr_fd(int number, int fd);
+void	ft_putstr_fd(char	*str, int fd);
 void	ft_putchar_fd(char symbol, int fd);
 void	ft_handler(int signum);
 void	ft_print(char bit);
