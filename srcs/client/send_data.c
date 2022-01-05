7b2944ff77f1b4fda00c086ddcd74b276d245d5d
Illarion <pro100illarion@gmail.com>
2022-01-05 18:39:42 +0300

    Added error handler

--- srcs/client/send_data.c
+++ srcs/client/send_data.c
@@ -6,11 +6,17 @@
 /*   By: glashli <glashli@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/05 13:36:02 by glashli           #+#    #+#             */
-/*   Updated: 2022/01/05 13:43:31 by glashli          ###   ########.fr       */
+/*   Updated: 2022/01/05 18:28:56 by glashli          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "client.h"
+
+void	ft_error_send_data(void)
+{
+	ft_putendl_fd("\nError: data don't send.", 2);
+	exit(EXIT_FAILURE);
+}
 
 void	ft_send_data(pid_t pid, char	*str)
 {
@@ -26,9 +32,12 @@ void	ft_send_data(pid_t pid, char	*str)
 		while (j)
 		{
 			if (j & str[i])
-				kill(pid, SIGUSR1); // добавить обработчик ошибки
-			else
-				kill(pid, SIGUSR2); // тоже
+			{
+				if (kill(pid, SIGUSR1) == -1)
+					ft_error_send_data();
+			}		
+			else if (kill(pid, SIGUSR2) == -1)
+				ft_error_send_data();
 			j /= 2;
 		}
 		i++;
