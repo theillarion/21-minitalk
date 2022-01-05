3af1060863750f3085bf0bf0e5cd2f6cdc3ccd9e
Illarion <pro100illarion@gmail.com>
2022-01-05 18:40:23 +0300

    Added new function prototypes

--- includes/client.h
+++ includes/client.h
@@ -6,7 +6,7 @@
 /*   By: glashli <glashli@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/05 13:36:38 by glashli           #+#    #+#             */
-/*   Updated: 2022/01/05 13:45:53 by glashli          ###   ########.fr       */
+/*   Updated: 2022/01/05 18:27:26 by glashli          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@
 
 size_t	ft_strlen(const char	*str);
 int		ft_atoi(const char	*str);
+void	ft_putendl_fd(char	*str, int fd);
+void	ft_error_send_data(void);
 void	ft_send_data(pid_t pid, char	*str);
 int		main(int argc, char	**argv);
 
